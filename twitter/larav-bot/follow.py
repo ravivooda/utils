c8489a70d0c6cd5d5de4e3d1de23b6d0b58522c0
Ravi Vooda <r.vooda@gmail.com>
2015-09-22 21:52:43 -0400

    Haha! Some what working model

--- twitter/larav-bot/follow.py
+++ twitter/larav-bot/follow.py
@@ -4,6 +4,10 @@ import Queue
 from keys import keys
 
 import time
+import json
+
+def pretty_print(dict):
+    print json.dumps(dict, sort_keys=True, indent=4)
  
 CONSUMER_KEY = keys['consumer_key']
 CONSUMER_SECRET = keys['consumer_secret']
@@ -17,11 +21,13 @@ api = tweepy.API(auth)
 
 print "Success Login \n\n\n\n"
 
+pretty_print(api.rate_limit_status())
+
 follow_q = []
  
 # First get my ollowers
 for follower in tweepy.Cursor(api.followers).items():
-    my_follower_name = follower.screen_name
+    my_follower_name = follower.id
     follow_q.append(my_follower_name)
 
 print "My Followers are: "
@@ -31,12 +37,24 @@ print follow_q
 print "\n\n\n\n\n ====================================== \n\n\n\n\n"
 
 i = 0; 
+me = api.me()
+print me
 while len(follow_q) != 0:
     follower_name = follow_q[0]
     print "\n\n Batch: " + str(i)
-    for new_follower in tweepy.Cursor(api.followers_ids, screen_name=follower_name).pages():
-        print new_follower
-        follow_q.extend(new_follower)
-        time.sleep(60)
+    print "Searching for: " + str(follower_name)
+    for new_follower in tweepy.Cursor(api.followers_ids, id=follower_name).pages():
+        for new_follower_id in new_follower:
+            if new_follower_id == me.id: # api.exists_friendship(me,new_follower_id):
+                print "Already friends: " + str(new_follower_id)
+                continue
+            try:
+                api.create_friendship(new_follower_id)
+                print "Success Man"
+            except tweepy.TweepError as e:
+                print e
+            print new_follower_id
+            follow_q.append(new_follower_id)
+            time.sleep(10)
     follow_q.pop(0)
     i = i+1
