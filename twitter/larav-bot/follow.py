93467d68982dbbaf2535d0ce5d451f186ae0c8cf
Ravi Vooda <r.vooda@gmail.com>
2016-01-10 20:55:19 +0000

    Well formatted code

--- twitter/larav-bot/follow.py
+++ twitter/larav-bot/follow.py
@@ -7,7 +7,25 @@ from keys import keys
 import time
 import json
 
+import requests.packages.urllib3
+
+
 logging.basicConfig(filename='log.txt',level=logging.DEBUG)
+
+# DISABLE OTHER USELESS LOGGING
+logging.getLogger("requests").setLevel(logging.CRITICAL)
+logging.getLogger("urllib3").setLevel(logging.CRITICAL)
+logging.getLogger("oauthlib").setLevel(logging.CRITICAL)
+logging.getLogger("tweepy").setLevel(logging.CRITICAL)
+logging.getLogger("requests_oauthlib").setLevel(logging.CRITICAL)
+requests.packages.urllib3.disable_warnings()
+
+
+logging.info ('''\n\n\n\n\n\n\n\n
+=========================================================================================================================================================================================================
+|                                                                               STARTING THE BOT. THIS IS A NEW CYCLE                                                                                   |
+=========================================================================================================================================================================================================
+''')
 
 CONSUMER_KEY = keys['consumer_key']
 CONSUMER_SECRET = keys['consumer_secret']
@@ -29,7 +47,7 @@ try:
         my_follower_name = follower.id
         follow_q.append(my_follower_name)
 except tweepy.TweepError:
-    logging.error("Rate limit exceeded. So just putting in my id: " + str(api.me().id))
+    logging.error("Rate limit exceeded. So just putting in my id: " + str(api.me().id) + "\n\n")
     follow_q.append(api.me().id)
 
 followed_q = {}
@@ -39,7 +57,9 @@ j = 0;
 z = 0;
 l = 0;
 me = api.me()
-logging.info("Well this is my data: " + str(me))
+logging.info("Well this is my data: " + str(me) + "\n\n")
+
+logging.info("Starting the actual bot")
 while len(follow_q) != 0:
     try:
         follower_name = follow_q[0]
@@ -49,12 +69,12 @@ while len(follow_q) != 0:
         for new_follower in tweepy.Cursor(api.followers_ids, id=follower_name).pages():
             for new_follower_id in new_follower:
                 logging.info("-------------------------------------------------------------------") #This is the divider for iterating through each of the follewers of the user
-                logging.info("User ID: " + new_follower_id)
+                logging.info("User ID: " + str(new_follower_id))
                 logging.info("Follow Queue List Length: " + str(len(follow_q)))
                 logging.info("Followed Queue List Length: " + str(len(followed_q)))
 
                 if new_follower_id in followed_q:
-                    logging.info("Current user: " + new_follower_id + " was already followed by the bot in this instance. Skipping for next guy immediately!")
+                    logging.info("Current user: " + str(new_follower_id) + " was already followed by the bot in this instance. Skipping for next guy immediately!")
                     z = z+1
                     continue
                     
@@ -67,16 +87,16 @@ while len(follow_q) != 0:
                 #logging.info("Friendship Data:\n" + api.show_friendship(target_id=new_follower_id))
                 try:
                     api.create_friendship(new_follower_id)
-                    logging.info("Successfully followed user: " + new_follower_id)
+                    logging.info("Successfully followed user: " + str(new_follower_id))
                 except tweepy.TweepError as e:
-                    logging.error("Unable to follow user: " + new_follower_id)
+                    logging.error("Unable to follow user: " + str(new_follower_id))
                     logging.error(e)
                     l=l+1;
                 if new_follower_id not in follow_q:
-                    logging.info("Added current user: " + new_follower_id + " to my queue")
-                    follow_q.append(new_follower_id)
+                    logging.info("Added current user: " + str(new_follower_id) + " to my queue")
+                    follow_q.append(str(new_follower_id))
                 else:
-                    logging.info("Already traversed loop with user: " + new_follower_id + ". Not adding to queue")
+                    logging.info("Already traversed loop with user: " + str(new_follower_id) + ". Not adding to queue")
                 time.sleep(60)
                 j = j+1
         follow_q.pop(0)
